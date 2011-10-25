778a98465182756a4d45958fbd738d0981d67bc8
rsa <kris13@bk.ru>
2011-10-25 14:48:25 +0400

    rem not need clear in destructor

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -11487,7 +11487,7 @@ void SpellAuraHolder::HandleSpellSpecificBoostsForward(bool apply)
 
 SpellAuraHolder::~SpellAuraHolder()
 {
-    m_aurasStorage.clear();
+//    m_aurasStorage.clear();
 //    DEBUG_LOG("SpellAuraHolder:: destructor for SpellAuraHolder of spell %u called.", GetId());
 }
 
