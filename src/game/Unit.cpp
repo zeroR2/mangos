d1d9ad4285c67d5b4151e344cce87b221417e37a
rsa <kris13@bk.ru>
2011-11-30 17:02:40 +0400

    rework applying SPELL_AURA_CONTROL_VEHICLE and some work with vehicles seating (auto-seat to accessories coming soon)

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -12211,7 +12211,7 @@ void Unit::ChangeSeat(int8 seatId, bool next)
 
 void Unit::EnterVehicle(VehicleKit *vehicle, int8 seatId)
 {
-    if (!isAlive() || GetVehicleKit() == vehicle)
+    if (!isAlive() || !vehicle || GetVehicleKit() == vehicle)
         return;
 
     if (m_pVehicle)
@@ -12225,6 +12225,14 @@ void Unit::EnterVehicle(VehicleKit *vehicle, int8 seatId)
         }
         else
             ExitVehicle();
+    }
+
+    if (seatId == -1)
+    {
+        if (vehicle->HasEmptySeat(-1))
+            seatId = vehicle->GetNextEmptySeat(0,true);
+        else
+            return;
     }
 
     InterruptNonMeleeSpells(false);
