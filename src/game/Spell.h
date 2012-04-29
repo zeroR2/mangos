3cd262a3654e524623914fc2064851cc72c2d790
rsa <kris13@bk.ru>
2012-04-29 10:53:47 +0400

    fix after last merge

--- src/game/Spell.h
+++ src/game/Spell.h
@@ -440,7 +440,6 @@ class Spell
         bool CheckTarget( Unit* target, SpellEffectIndex eff );
         SpellCastResult CanAutoCast(Unit* target);
         bool CheckTargetBeforLimitation(Unit* target);
-        bool CanAutoCast(Unit* target);
 
         static void MANGOS_DLL_SPEC SendCastResult(Player* caster, SpellEntry const* spellInfo, uint8 cast_count, SpellCastResult result);
         void SendCastResult(SpellCastResult result);
