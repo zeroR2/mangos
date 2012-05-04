3463503f93058077430dadcafb33b466c860d7e2
rsa <kris13@bk.ru>
2012-05-04 09:16:49 +0400

    Revert "Rewrite EventProcessor on LockedVector base, v0.6 (most problems eliminated). related to #829"
    This reverts commit 90e67ef41e25bb6c6198a9f0e1971b61a0de804e.

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -13544,16 +13544,26 @@ EventProcessor* Unit::GetEvents()
 
 void Unit::KillAllEvents(bool force)
 {
+    MAPLOCK_WRITE(this, MAP_LOCK_TYPE_DEFAULT);
     GetEvents()->KillAllEvents(force);
 }
 
 void Unit::AddEvent(BasicEvent* Event, uint64 e_time, bool set_addtime)
 {
-    GetEvents()->AddEvent(Event, GetEvents()->CalculateTime(e_time), set_addtime);
+    MAPLOCK_WRITE(this, MAP_LOCK_TYPE_DEFAULT);
+    if (set_addtime)
+        GetEvents()->AddEvent(Event, GetEvents()->CalculateTime(e_time), set_addtime);
+    else
+        GetEvents()->AddEvent(Event, e_time, set_addtime);
 }
 
 void Unit::UpdateEvents(uint32 update_diff, uint32 time)
 {
+    {
+        MAPLOCK_READ(this, MAP_LOCK_TYPE_DEFAULT);
+        GetEvents()->RenewEvents();
+    }
+
     GetEvents()->Update(update_diff);
 }
 
