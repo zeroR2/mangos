e908e0b8f9d4dbd7865fda8c7316b20145fbe98b
rsa <kris13@bk.ru>
2012-05-13 21:25:46 +0400

    Revert "Rewrite EventProcessor on LockedVector base, v0.10. related to #829"
    This reverts commit afd74ba34ce24d6807ca2148f78c1a3bf20d72b0.

--- src/framework/Utilities/EventProcessor.h
+++ src/framework/Utilities/EventProcessor.h
@@ -19,11 +19,10 @@
 #ifndef __EVENTPROCESSOR_H
 #define __EVENTPROCESSOR_H
 
-#include "ObjectHandler.h"
-#include "LockedVector.h"
-#include "ace/Null_Mutex.h"
-#include "Log.h"
-#include "Timer.h"
+#include "Platform/Define.h"
+
+#include <map>
+#include <queue>
 
 // Note. All times are in milliseconds here.
 
@@ -32,7 +31,7 @@ class BasicEvent
     public:
 
         BasicEvent()
-            : to_Abort(false), finished(false)
+            : to_Abort(false)
         {
         }
 
@@ -47,24 +46,18 @@ class BasicEvent
 
         virtual bool IsDeletable() const { return true; }   // this event can be safely deleted
 
-        virtual void Abort(uint32 /*e_time*/) {}            // this method executes when the event is aborted
+        virtual void Abort(uint64 /*e_time*/) {}            // this method executes when the event is aborted
 
-        virtual void Finish() { finished = true;}           // this method executes when the event is aborted
+        bool to_Abort;                                      // set by externals when the event is aborted, aborted events don't execute
+        // and get Abort call when deleted
 
         // these can be used for time offset control
-        void ModExecTime(uint32 e_time = 0);
-        bool IsFinished() const     { return finished;}     // check if event finished
-
-        uint32 m_addTime;                                   // time when the event was added to queue, filled by event handler
-        uint32 m_execTime;                                  // planned time of next execution, filled by event handler
-        bool   to_Abort;                                    // set by externals when the event is aborted, aborted events don't execute
-
-    private:
-        bool   finished;                                    // set external where event completely executed (for speedup cleanup)
+        uint64 m_addTime;                                   // time when the event was added to queue, filled by event handler
+        uint64 m_execTime;                                  // planned time of next execution, filled by event handler
 };
 
-OBJECT_SAFE_HANDLER(BasicEvent,BasicEventPtr);
-typedef ACE_Based::LockedVector<BasicEventPtr> EventList;
+typedef std::multimap<uint64, BasicEvent*> EventList;
+typedef std::queue<std::pair<uint64, BasicEvent*> > EventNewQueue;
 
 class EventProcessor
 {
@@ -73,14 +66,17 @@ class EventProcessor
         EventProcessor();
         ~EventProcessor();
 
-        void Update(uint32 update_diff, uint32 p_time);
+        void Update(uint32 p_time, bool force = false);
         void KillAllEvents(bool force);
-        void AddEvent(BasicEvent* Event, uint32 e_time, bool set_addtime = true);
-        uint32 CalculateTime(uint32 t_offset);
-        void AddEvent(BasicEventPtr Event);
+        void AddEvent(BasicEvent* Event, uint64 e_time, bool set_addtime = true);
+        uint64 CalculateTime(uint64 t_offset);
+        void RenewEvents();
 
     protected:
+        void _AddEvents();
+        uint64 m_time;
         EventList m_events;
+        EventNewQueue m_queue;
         bool m_aborting;
 };
 
