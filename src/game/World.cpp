ea473e82562bc343100e7eb0fcd572f413b74359
rsa <kris13@bk.ru>
2012-07-19 16:20:34 +0400

    implement and use CONFIG_BOOL_DYNAMIC_VMAP_DOUBLE_CHECK value for test dual-sided GO LOS checking. Attention - may require much CPU usage!. testing.

--- src/game/World.cpp
+++ src/game/World.cpp
@@ -873,6 +873,8 @@ void World::LoadConfigSettings(bool reload)
 
     setConfig(CONFIG_BOOL_ALLOW_FLIGHT_ON_OLD_MAPS, "AllowFlightOnOldMaps", false);
 
+    setConfig(CONFIG_BOOL_DYNAMIC_VMAP_DOUBLE_CHECK,"vmap.Dynamic.DoubleCheck", false);
+
     m_relocation_ai_notify_delay = sConfig.GetIntDefault("Visibility.AIRelocationNotifyDelay", 1000u);
     m_relocation_lower_limit_sq  = pow(sConfig.GetFloatDefault("Visibility.RelocationLowerLimit",10), 2);
 
