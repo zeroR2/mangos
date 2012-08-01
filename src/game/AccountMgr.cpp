97530da67d0890a3a7e0a2bbd4143581f5f2e494
rsa <kris13@bk.ru>
2012-08-01 09:53:39 +0400

    Revert "some corrects to RAF system. related to #1100,testing."
    This reverts commit 536b847a1ec4a585cd2d4d63eaefe44343354f60.

--- src/game/AccountMgr.cpp
+++ src/game/AccountMgr.cpp
@@ -290,11 +290,11 @@ AccountOpResult AccountMgr::AddRAFLink(uint32 accid, uint32 friendid)
 
     RafLinkedList* referred = GetRAFAccounts(accid, true);
     if (referred)
-        referred->push_back(friendid);
+        referred->push_back(accid);
 
     RafLinkedList* referal = GetRAFAccounts(friendid, false);
     if (referal)
-        referal->push_back(accid);
+        referal->push_back(friendid);
 
     return AOR_OK;
 }
@@ -392,20 +392,10 @@ void AccountMgr::ClearPlayerDataCache(ObjectGuid guid)
     if (!guid || !guid.IsPlayer())
         return;
 
-    uint32 accId = GetPlayerAccountIdByGUID(guid);
-
     WriteGuard Guard(GetLock());
     PlayerDataCacheMap::iterator itr = mPlayerDataCacheMap.find(guid);
     if (itr != mPlayerDataCacheMap.end())
         mPlayerDataCacheMap.erase(itr);
-
-    RafLinkedMap::iterator itr1 = mRAFLinkedMap.find(std::pair<uint32,bool>(accId, true));
-    if (itr1 != mRAFLinkedMap.end())
-        mRAFLinkedMap.erase(itr1);
-    itr1 = mRAFLinkedMap.find(std::pair<uint32,bool>(accId, false));
-    if (itr1 != mRAFLinkedMap.end())
-        mRAFLinkedMap.erase(itr1);
-
 }
 
 void AccountMgr::MakePlayerDataCache(Player* player)
