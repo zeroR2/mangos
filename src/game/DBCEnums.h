8033cc007c9668abd788b69df42f961258371b41
rsa <kris13@bk.ru>
2012-08-16 15:00:29 +0400

    Revert "implement SmartPtr part 3, stage 1 v0. - rewrite per-unit Aura* links storage."
    This reverts commit f7b90dcecf6ffd0b2623055e373f48c9d6f1ab09.

--- src/game/DBCEnums.h
+++ src/game/DBCEnums.h
@@ -450,10 +450,10 @@ enum SpellEffectIndex
 {
     EFFECT_INDEX_0     = 0,
     EFFECT_INDEX_1     = 1,
-    EFFECT_INDEX_2     = 2,
-    MAX_EFFECT_INDEX,
+    EFFECT_INDEX_2     = 2
 };
 
+#define MAX_EFFECT_INDEX 3
 
 enum SpellFamily
 {
