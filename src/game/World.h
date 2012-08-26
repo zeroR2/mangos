e51ecf02d3e443df5c0cd2ebfd308b7a3d7a9bbb
rsa <kris13@bk.ru>
2012-08-26 16:23:10 +0400

    move BattleGround code to separate dir (as OutdoorPvP). also some corrects to unload BG's

--- src/game/World.h
+++ src/game/World.h
@@ -515,6 +515,8 @@ class World
         World();
         ~World();
 
+        void CleanupsBeforeStop();
+
         WorldSession* FindSession(uint32 id) const;
         void AddSession(WorldSession *s);
         void SendBroadcast();
