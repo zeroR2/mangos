4260d7af47cae385b7ab8ccfa507b055eaf93ead
rsa <kris13@bk.ru>
2012-09-01 15:20:04 +0400

    small fixes related to pet evading (need test!): - remove old tempfix in StateMgr (HOME action must have lower priority, then CHASE) - correct define pet evading state - make evade action for pets related to current command mode

--- src/game/SpellAuras.cpp
+++ src/game/SpellAuras.cpp
@@ -4957,7 +4957,7 @@ void Aura::HandleModPossessPet(bool apply, bool Real)
         }
         else
         {
-            pet->GetMotionMaster()->MoveFollow(caster, PET_FOLLOW_DIST, PET_FOLLOW_ANGLE);
+            pet->GetMotionMaster()->MoveTargetedHome();
         }
     }
 }
