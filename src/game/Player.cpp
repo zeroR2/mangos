9e206e95d79b1da8dc84ed1df968b885596786ad
rsa <kris13@bk.ru>
2012-10-03 17:24:58 +0400

    starting TeleportMgr implement - corrects, related to Object Loading splitter - make PreloadGrid method and use for detect fully-loaded grids, possible for teleport - make PlayerEvent TeleportDelayEvent - check grid loaded stage while teleport process Testing.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -1729,35 +1729,35 @@ uint8 Player::GetChatTag() const
     return tag;
 }
 
-bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientation, uint32 options)
+bool Player::TeleportTo(WorldLocation const& loc, uint32 options)
 {
-    if(!MapManager::IsValidMapCoord(mapid, x, y, z, orientation))
+    if (!MapManager::IsValidMapCoord(loc))
     {
-        sLog.outError("TeleportTo: invalid map %d or absent instance template.", mapid);
+        sLog.outError("TeleportTo: invalid map %d or absent instance template.", loc.mapid);
         return false;
     }
 
-    if (GetMapId() != mapid)
+    if (GetMapId() != loc.mapid)
     {
         if (!(options & TELE_TO_CHECKED))
         {
-            if (!CheckTransferPossibility(mapid))
+            if (!CheckTransferPossibility(loc.mapid))
             {
                 if (GetTransport())
                     TeleportToHomebind();
 
-                DEBUG_LOG("Player::TeleportTo %s is NOT teleported to map %u (requirements check failed)", GetName(), mapid);
+                DEBUG_LOG("Player::TeleportTo %s is NOT teleported to map %u (requirements check failed)", GetName(), loc.mapid);
 
                 return false;                                       // normal client can't teleport to this map...
             }
             else
                 options |= TELE_TO_CHECKED;
         }
-        DEBUG_LOG("Player::TeleportTo %s is being far teleported to map %u", GetName(), mapid);
+        DEBUG_LOG("Player::TeleportTo %s is being far teleported to map %u %s", GetName(), loc.mapid, IsBeingTeleportedFar() ? "(stage 2)" : "");
     }
     else
     {
-        DEBUG_LOG("Player::TeleportTo %s is being near teleported to map %u", GetName(), mapid);
+        DEBUG_LOG("Player::TeleportTo %s is being near teleported to map %u", GetName(), loc.mapid, IsBeingTeleportedNear() ? "(stage 2)" : "");
     }
 
     // preparing unsummon pet if lost (we must get pet before teleportation or will not find it later)
@@ -1768,11 +1768,11 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     if (GetPlayerbotMgr())
         GetPlayerbotMgr()->Stay();
 
-    MapEntry const* mEntry = sMapStore.LookupEntry(mapid);
+    MapEntry const* mEntry = sMapStore.LookupEntry(loc.mapid);
 
     if(!mEntry)
     {
-        sLog.outError("TeleportTo: invalid map entry (id %d). possible disk or memory error.", mapid);
+        sLog.outError("TeleportTo: invalid map entry (id %d). possible disk or memory error.", loc.mapid);
         return false;
     }
 
@@ -1784,7 +1784,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     // client without expansion support
 
     if (Group* grp = GetGroup())
-        grp->SetPlayerMap(GetObjectGuid(), mapid);
+        grp->SetPlayerMap(GetObjectGuid(), loc.mapid);
 
     // if we were on a transport, leave
     if (!(options & TELE_TO_NOT_LEAVE_TRANSPORT) && m_transport)
@@ -1802,7 +1802,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     // The player was ported to another map and looses the duel immediately.
     // We have to perform this check before the teleport, otherwise the
     // ObjectAccessor won't find the flag.
-    if (duel && GetMapId() != mapid)
+    if (duel && GetMapId() != loc.mapid)
         if (GetMap()->GetGameObject(GetGuidValue(PLAYER_DUEL_ARBITER)))
             DuelComplete(DUEL_FLED);
 
@@ -1810,10 +1810,21 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
     m_movementInfo.SetMovementFlags(MOVEFLAG_NONE);
     DisableSpline();
 
-    if (GetMapId() == mapid && !m_transport)
+    if (GetMapId() == loc.mapid && !m_transport)
     {
         //lets reset far teleport flag if it wasn't reset during chained teleports
         SetSemaphoreTeleportFar(false);
+
+        // try preload grid, targeted for teleport
+        if (!GetMap()->PreloadGrid(loc.coord_x, loc.coord_y))
+        {
+            // If loading grid not finished, delay teleport on one update tick
+            AddEvent(new TeleportDelayEvent(*this, loc, options),
+                sWorld.getConfig(CONFIG_UINT32_INTERVAL_MAPUPDATE));
+            DEBUG_LOG("Player::TeleportTo grid (map %u, instance %u, X%f Y%f) not fully loaded, near teleport %s delayed.", loc.mapid, GetInstanceId(), loc.coord_x, loc.coord_y, GetName());
+            return true;
+        }
+
         //setup delayed teleport flag
         //if teleport spell is casted in Unit::Update() func
         //then we need to delay it until update process will be finished
@@ -1821,7 +1832,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
         {
             SetSemaphoreTeleportNear(true);
             //lets save teleport destination for player
-            m_teleport_dest = WorldLocation(mapid, x, y, z, orientation);
+            m_teleport_dest = loc;
             m_teleport_options = options;
             return true;
         }
@@ -1829,7 +1840,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
         if (!(options & TELE_TO_NOT_UNSUMMON_PET))
         {
             //same map, only remove pet if out of range for new position
-            if (pet && !pet->IsWithinDist3d(x, y, z, GetMap()->GetVisibilityDistance()))
+            if (pet && !pet->IsWithinDist3d(loc.x, loc.y, loc.z, GetMap()->GetVisibilityDistance()))
                 UnsummonPetTemporaryIfAny();
         }
 
@@ -1837,8 +1848,8 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             CombatStop();
 
         // this will be used instead of the current location in SaveToDB
-        m_teleport_dest = WorldLocation(mapid, x, y, z, orientation);
-        SetFallInformation(0, z);
+        m_teleport_dest = loc;
+        SetFallInformation(0, loc.coord_z);
 
         // code for finish transfer called in WorldSession::HandleMovementOpcodes()
         // at client packet MSG_MOVE_TELEPORT_ACK
@@ -1847,7 +1858,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
         if(!GetSession()->PlayerLogout())
         {
             WorldPacket data;
-            BuildTeleportAckMsg(data, x, y, z, orientation);
+            BuildTeleportAckMsg(data, loc.coord_x, loc.coord_y, loc.coord_z, loc.orientation);
             GetSession()->SendPacket(&data);
         }
     }
@@ -1859,17 +1870,40 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
 
         // Check enter rights before map getting to avoid creating instance copy for player
         // this check not dependent from map instance copy and same for all instance copies of selected map
-        if (!sMapMgr.CanPlayerEnter(mapid, this))
+        if (!sMapMgr.CanPlayerEnter(loc.mapid, this))
             return false;
 
         // If the map is not created, assume it is possible to enter it.
         // It will be created in the WorldPortAck.
-        DungeonPersistentState* state = GetBoundInstanceSaveForSelfOrGroup(mapid);
-        Map *map = sMapMgr.FindMap(mapid, state ? state->GetInstanceId() : 0);
+        DungeonPersistentState* state = GetBoundInstanceSaveForSelfOrGroup(loc.mapid);
+        Map* map = sMapMgr.FindMap(loc.mapid, state ? state->GetInstanceId() : loc.instance);
         if (!map ||  map->CanEnter(this))
         {
             //lets reset near teleport flag if it wasn't reset during chained teleports
             SetSemaphoreTeleportNear(false);
+
+            // try create map before trying teleport on this map
+            if (!map)
+            {
+                if (mEntry->Instanceable() && loc.instance)
+                    map = sMapMgr.FindMap(loc.mapid, loc.instance);
+                else
+                    map = sMapMgr.CreateMap(loc.mapid, this);
+
+                MANGOS_ASSERT(map);
+            }
+
+            // try preload grid, targeted for teleport
+            if (!map->PreloadGrid(loc.coord_x, loc.coord_y))
+            {
+                // If loading grid not finished, delay teleport 5 map update ticks
+                AddEvent(new TeleportDelayEvent(*this, WorldLocation(loc.coord_x, loc.coord_y, loc.coord_z, loc.orientation, loc.mapid, map->GetInstanceId(), realmID), options),
+                    5 * sWorld.getConfig(CONFIG_UINT32_INTERVAL_MAPUPDATE));
+
+                DEBUG_LOG("Player::TeleportTo grid (map %u, instance %u, X%f Y%f) not fully loaded, far teleport %s delayed.", loc.mapid, map->GetInstanceId(), loc.coord_x, loc.coord_y, GetName());
+                return true;
+            }
+
             //setup delayed teleport flag
             //if teleport spell is casted in Unit::Update() func
             //then we need to delay it until update process will be finished
@@ -1877,7 +1911,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             {
                 SetSemaphoreTeleportFar(true);
                 //lets save teleport destination for player
-                m_teleport_dest = WorldLocation(mapid, x, y, z, orientation);
+                m_teleport_dest = loc;
                 m_teleport_options = options;
                 return true;
             }
@@ -1894,7 +1928,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
                 // Note: at battleground join battleground id set before teleport
                 // and we already will found "current" battleground
                 // just need check that this is targeted map or leave
-                if (bg->GetMapId() != mapid)
+                if (bg->GetMapId() != loc.mapid)
                     LeaveBattleground(false);                   // don't teleport to entry point
             }
 
@@ -1917,7 +1951,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             {
                 // send transfer packet to display load screen
                 WorldPacket data(SMSG_TRANSFER_PENDING, (4+4+4));
-                data << uint32(mapid);
+                data << uint32(loc.mapid);
                 if (m_transport)
                 {
                     data << uint32(m_transport->GetEntry());
@@ -1931,10 +1965,10 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
                 oldmap->Remove(this, false);
 
             // new final coordinates
-            float final_x = x;
-            float final_y = y;
-            float final_z = z;
-            float final_o = orientation;
+            float final_x = loc.coord_x;
+            float final_y = loc.coord_y;
+            float final_z = loc.coord_z;
+            float final_o = loc.orientation;
 
             if (m_movementInfo.HasMovementFlag(MOVEFLAG_ONTRANSPORT))
             {
@@ -1944,7 +1978,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
                 final_o += m_movementInfo.GetTransportPos()->o;
             }
 
-            m_teleport_dest = WorldLocation(mapid, final_x, final_y, final_z, final_o);
+            m_teleport_dest = WorldLocation(loc.mapid, final_x, final_y, final_z, final_o);
             SetFallInformation(0, final_z);
             // if the player is saved before worldport ack (at logout for example)
             // this will be used instead of the current location in SaveToDB
@@ -1957,7 +1991,7 @@ bool Player::TeleportTo(uint32 mapid, float x, float y, float z, float orientati
             {
                 // transfer finished, inform client to start load
                 WorldPacket data(SMSG_NEW_WORLD, (20));
-                data << uint32(mapid);
+                data << uint32(loc.mapid);
                 if (m_transport)
                 {
                     data << float(m_movementInfo.GetTransportPos()->x);
